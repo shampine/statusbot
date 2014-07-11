7d4d3e01204eac0a5572012b448602492e4ac83a
Patrick Shampine <pshampine@gunnjerkens.com>
2014-07-11 10:42:40 -0700

    Remove errant tag

--- public/inc/header.php
+++ public/inc/header.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /><?php 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="robots" content="noindex, nofollow">
     <title><?php echo theTitle(); ?></title>
     <link rel="shortcut icon" href="img/favicon.png" />
