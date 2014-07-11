852beb8743d3ebddae9c15084f184b868efc82af
Patrick Shampine <pshampine@gunnjerkens.com>
2014-07-11 10:41:20 -0700

    Robots no follow and remove js

--- public/inc/footer.php
+++ public/inc/footer.php
@@ -1,10 +1,9 @@
-    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
-    <script src="js/main.js"></script><?php
+<?php
 
-    if ($environment === 'dev') {
-     echo "<script src='http://localhost:3000/socket.io/socket.io.js'></script>";
-     echo "<script src='http://localhost:3001/browser-sync-client.min.js'></script>";
-    } ?>
+  if ($environment === 'dev') {
+   echo "<script src='http://localhost:3000/socket.io/socket.io.js'></script>";
+   echo "<script src='http://localhost:3001/browser-sync-client.min.js'></script>";
+  } ?>
 
   </body>
 </html>
