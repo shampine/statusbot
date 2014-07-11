e6e410786c2fdef177431d792c63f3e25830b1b0
Ryan Gordon <rgordon@gunnjerkens.com>
2014-07-11 16:33:40 -0700

    Animnations?
    git commit -am Animnations?

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php include_once('inc/header.php'); ?>
 
   <div class="container">
-    <p class="updated">Updated: <?php echo date('D g:i a', $monitor_response_time); ?></p>
+    <p class="updated animated flash">Updated: <?php echo date('D g:i a', $monitor_response_time); ?></p>
     <div class="table-responsive">
       <table class="table">
         <thead>
-          <tr>
+          <tr class="animated fadeInUp">
             <th>Server</th>
             <th>Current Status</th>
             <th><?php echo date('D m.d',strtotime("-1 days")); ?></th>
@@ -21,11 +21,11 @@
           foreach($servers as $server) {
 
             echo '<tr>';
-            echo '<th>'.$server['friendlyname'].'</th>';
-            echo '<th>'.($server['status'] === "2" ? '<i class="fa fa-check-circle"></i> Online' : '<i class="fa fa-times-circle"></i> Offline').'</th>';
+            echo '<th class="animated flipInX">'.$server['friendlyname'].'</th>';
+            echo '<th class="animated flipInX">'.($server['status'] === "2" ? '<i class="fa fa-check-circle"></i> Online' : '<i class="fa fa-times-circle"></i> Offline').'</th>';
 
             foreach($server["ratios"] as $ratio) {
-              echo '<th>';
+              echo '<th class="animated flipInX">';
 
               if($ratio === 'green') {
 
@@ -56,7 +56,7 @@
     <div class="table-responsive">
       <table class="table">
         <thead>
-          <tr>
+          <tr class="animated fadeInUp">
             <th>Site</th>
             <th>Current Status</th>
             <th><?php echo date('D m.d',strtotime("-1 days")); ?></th>
@@ -72,11 +72,11 @@
           foreach($sites as $site) {
 
             echo '<tr>';
-            echo '<th><a href="'.$site['url'].'" target="_blank">'.$site['friendlyname'].'</a></th>';
-            echo '<th>'.($site['status'] === "2" ? '<i class="fa fa-check-circle"></i> Online' : '<i class="fa fa-times-circle"></i> Offline').'</th>';
+            echo '<th class="animated flipInX"><a href="'.$site['url'].'" target="_blank">'.$site['friendlyname'].'</a></th>';
+            echo '<th class="animated flipInX">'.($site['status'] === "2" ? '<i class="fa fa-check-circle"></i> Online' : '<i class="fa fa-times-circle"></i> Offline').'</th>';
 
             foreach($site["ratios"] as $ratio) {
-              echo '<th>';
+              echo '<th class="animated flipInX">';
 
               if($ratio === 'green') {
 
