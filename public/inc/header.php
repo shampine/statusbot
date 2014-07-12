e607df992e39788a7b5a0c4b6194f404b35f7ad9
patrickshampine <patrick@patrickshampine.com>
2014-07-12 14:38:22 -0700

    Removed branding

--- public/inc/header.php
+++ public/inc/header.php
@@ -3,8 +3,8 @@
 <html class="no-js">
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="robots" content="noindex, nofollow">
     <title><?php echo theTitle(); ?></title>
     <link rel="shortcut icon" href="img/favicon.png" />
@@ -13,7 +13,6 @@
     $stylesheet = 'style/screen.css?' . filemtime('style/screen.css'); ?>
     <link rel="stylesheet" href="<?php echo $stylesheet; ?>">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <script type='text/javascript' src='js/modernizr.js'></script>
     <?php echo theAnalytics(); ?>
   </head>
   <body>
